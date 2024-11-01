74d7edb54b163178e08e01dc04be2c497fc39ee8
five76 <fiv_e76@mail.ru>
2024-11-01 17:03:34 +0500

    Update Add os

--- hello_git.py
+++ hello_git.py
@@ -1,5 +1,8 @@
+import os
 print('Hello, Git!')
 print('Индексация - это важно')
 print('Create new branch testing')
 for num in range(10):
         print(num,end=' ')
+        
+print(f'\nYour current directory: {os.getcwd()}')
