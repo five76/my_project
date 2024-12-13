d6a63f6f62b0ca582fca1d6492c939d884d45b36
five76 <fiv_e76@mail.ru>
2024-12-13 13:06:43 +0500

    Update hg. Add module.py

--- hello_git.py
+++ hello_git.py
@@ -1,4 +1,5 @@
 import os
+import module
 print('Hello, Git!')
 print("Indexing it's important!!!")
 
@@ -10,3 +11,4 @@ print(f'\nYour current directory: {os.getcwd()}')
 s = 'Version Control System'
 print(f'{s} - length is {len(s)} characters')
 
+print(module.add_num(3,7))
