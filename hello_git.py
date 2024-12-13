7f8696507a8109226725470c16bc87045d0fd41d
five76 <fiv_e76@mail.ru>
2024-12-13 12:57:32 +0500

    Update hello_git py. Add about VCS

--- hello_git.py
+++ hello_git.py
@@ -1,6 +1,12 @@
+import os
 print('Hello, Git!')
 print("Indexing it's important!!!")
 
 for num in range(10):
         print(num,end=' ')
+
+print(f'\nYour current directory: {os.getcwd()}')
+
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
 
