d6793dafc2b65509e76ca6cc491466df0a8a3205
five76 <fiv_e76@mai.ru>
2025-05-23 17:26:51 +0500

    Upd. Add about VCS

--- hello_git.py
+++ hello_git.py
@@ -1,6 +1,13 @@
+import os
+
 print('Hello, Git!')
 print('Indexing - it\'s important')
 print('Create new branch testing') 
 
 for num in range(10):
      print(num,end=' ')
+
+print(f'\nYour current directory: {os.getcwd()}')
+
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
