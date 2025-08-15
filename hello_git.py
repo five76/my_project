4f9a684102947771e7c97d470132db98b3d8167c
five76 <fiv_e76@mail.ru>
2025-08-15 13:12:53 +0500

    Update hello_git py. Add module b

--- hello_git.py
+++ hello_git.py
@@ -1,4 +1,5 @@
 import os
+import b
 
 print('Hello, Git!')
 print('Indexing it\'s important')
@@ -11,3 +12,5 @@ print(f'\nYour current directory: {os.getcwd()}')
 
 s = 'Version Control System'
 print(f'{s} - length is {len(s)} characters')
+
+print(b.sum_numbers(1,2,3,4,5))
